ee2a0df0cb1fb242a152af2334b687582b9a7fdf
Scott Willeke <scott@willeke.com>
2013-03-26 23:48:08 -0700

    this fixes issue 48 using powershell script instead.
    fixes issue 48

--- src/.build/chocolateyInstall.ps1
+++ src/.build/chocolateyInstall.ps1
@@ -1,1 +1,15 @@
-Install-ChocolateyZipPackage 'LessMsi' '__URL__' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+try {
+	
+	$theFile = Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) "AddWindowsExplorerShortcut.exe.ignore"
+	Write-Host "Creating " $theFile
+	New-Item $theFile -type file
+
+	Install-ChocolateyZipPackage 'LessMsi' '__URL__' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+	Write-ChocolateySuccess 'lessmsi'
+
+} catch {
+	Write-ChocolateyFailure 'lessmsi' $($_.Exception.Message)
+	throw 
+}
