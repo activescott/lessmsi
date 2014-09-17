c45fc608eb029c061b0c9a6cde5d9ba7336cbb41
Scott Willeke <scott@willeke.com>
2014-09-16 21:07:58 -0700

    ensure tests use netfx 3.5 too

--- src/Lessmsi.Tests/CommandLineExtractTests.cs
+++ src/Lessmsi.Tests/CommandLineExtractTests.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Runtime.Serialization.Json;
-using System.Text;
 using NUnit.Framework;
 
 namespace LessMsi.Tests
