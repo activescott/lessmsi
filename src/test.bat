cbe2d3e955d9814a41407847b899c6e420d2ad95
Scott Willeke <scott@willeke.com>
2016-04-24 20:09:29 -0700

    vs2015/v14 support. removed ZipFile task in favor of Zip task from msbuild commnity tasks

--- src/test.bat
+++ src/test.bat
@@ -2,4 +2,4 @@
 @set /P CONFIG= Enter the config to test with (Release or Debug, defaults to Release): 
 @if NOT DEFINED CONFIG set CONFIG=Release
 
-.\packages\NUnit.Runners.2.6.3\tools\nunit-console.exe .\Lessmsi.Tests\bin\%CONFIG%\LessMsi.Tests.dll /labels
+.\packages\NUnit.ConsoleRunner.3.2.1\tools\nunit3-console.exe .\Lessmsi.Tests\bin\%CONFIG%\LessMsi.Tests.dll /labels=All
