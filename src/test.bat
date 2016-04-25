11b6a6df9846879226a7fe9f42c713e3514db914
Scott Willeke <scott@willeke.com>
2016-04-24 20:52:12 -0700

    fixed a case where test.bat picks up a global CONFIG value.

--- src/test.bat
+++ src/test.bat
@@ -1,5 +1,6 @@
-
+@setlocal
+@set CONFIG=
 @set /P CONFIG= Enter the config to test with (Release or Debug, defaults to Release): 
 @if NOT DEFINED CONFIG set CONFIG=Release
-
+echo Config: %CONFIG%
 .\packages\NUnit.ConsoleRunner.3.2.1\tools\nunit3-console.exe .\Lessmsi.Tests\bin\%CONFIG%\LessMsi.Tests.dll /labels=All
