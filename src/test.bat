b8b0bc9d9ae5aff41a8a7fed827200417e555bed
Scott Willeke <scott@willeke.com>
2016-05-29 22:45:46 -0700

    can run tests from deploy dir now

--- src/test.bat
+++ src/test.bat
@@ -1,9 +1,3 @@
 @setlocal
-@set CONFIG=
-@set /P CONFIG= Enter the config to test with (Release or Debug, defaults to Release): 
-@if NOT DEFINED CONFIG set CONFIG=Release
-echo Config: %CONFIG%
 
-@set PAUSEFORDEBUG=
-REM @set PAUSEFORDEBUG=--debug
-packages\xunit.runner.console.2.1.0\tools\xunit.console.x86.exe .\Lessmsi.Tests\bin\%CONFIG%\LessMsi.Tests.dll %PAUSEFORDEBUG%
+packages\xunit.runner.console.2.1.0\tools\xunit.console.x86.exe .\.deploy\LessMsi.Tests.dll
