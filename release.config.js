0d98d29a09bbe99f8c832ae9d9a00c3095ce4fb6
Scott Willeke <scott@willeke.com>
2020-10-10 15:33:52 -0700

    build: paths for windows

--- release.config.js
+++ release.config.js
@@ -10,15 +10,15 @@ module.exports = {
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     ["@semantic-release/exec", {
-      "verifyConditionsCmd": "./src/.build/semantic-release-verify.cmd",
-      "prepareCmd": "./src/.build/semantic-release-prepare.cmd ${nextRelease.version}",
-      "publishCmd": "./src/.build/semantic-release-publish.cmd",
+      "verifyConditionsCmd": "src\\.build\\semantic-release-verify.cmd",
+      "prepareCmd": "src\\.build\\semantic-release-prepare.cmd ${nextRelease.version}",
+      "publishCmd": "src\\.build\\semantic-release-publish.cmd",
     }],
     // github config docs: https://github.com/semantic-release/github
     ["@semantic-release/github", {
       "assets": [
-        {"path": "src\.deploy\chocolateypackage\*.nupkg", "label": "NuGet distribution"},
-        {"path": "src\.deploy\*.zip", "label": "ZIP distribution"}
+        {"path": "src\\.deploy\\chocolateypackage\\*.nupkg", "label": "NuGet distribution"},
+        {"path": "src\\.deploy\\*.zip", "label": "ZIP distribution"}
       ]
     }],
   ]
