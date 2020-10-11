33cce659f178cc28dfa8a3327a34d3d308637bd3
Scott Willeke <scott@willeke.com>
2020-10-11 11:19:53 -0700

    fix: chocolatey package iconUrl; update release notes
    * closes #146 because 8fa29c869fff040c6aac99cfc9a925a4d7471034 and this just republishing the package
    * fixes #145 by updating the path to *nix globs instead of windows paths
    * closes #147 by using footerPartial config

--- release.config.js
+++ release.config.js
@@ -4,7 +4,23 @@ module.exports = {
   ],
   plugins: [
     "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
+    ["@semantic-release/release-notes-generator", {
+      writerOpts: {
+        footerPartial: `{{#if noteGroups}}
+        {{#each noteGroups}}
+        
+        ### {{title}}
+        
+        {{#each notes}}
+        * {{text}}
+        {{/each}}
+        {{/each}}
+        {{/if}}
+
+        You can install or upgrade by extracting all files from the zip attached to this release into a single directory or via [Chocolatey](https://chocolatey.org/packages/lessmsi).
+        `
+      }
+    }],
     ["@semantic-release/exec", {
       "verifyConditionsCmd": "src\\.build\\semantic-release-verify.cmd",
       "prepareCmd": "src\\.build\\semantic-release-prepare.cmd ${nextRelease.version}",
@@ -13,8 +29,8 @@ module.exports = {
     // github config docs: https://github.com/semantic-release/github
     ["@semantic-release/github", {
       "assets": [
-        {"path": "src\\.deploy\\chocolateypackage\\*.nupkg", "label": "NuGet distribution"},
-        {"path": "src\\.deploy\\*.zip", "label": "ZIP distribution"}
+        {"path": "src/.deploy/chocolateypackage/*.nupkg", "label": "Chocolatey Package"},
+        {"path": "src/.deploy/*.zip", "label": "Zip of lessmsi application binaries"}
       ]
     }],
   ]
