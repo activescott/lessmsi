affc1c71e373b15b213ea78f7dbdf0a986813afd
scott willeke <scott@willeke.com>
2020-10-10 22:49:53 -0700

    chore(build): semantic-release now used for deployments
    Using semantic-release for deployments based on conventional commits and Chocolatey deployments & GitHub Releases release notes

---
+++ release.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  branches: [
+    "master"
+  ],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    ["@semantic-release/exec", {
+      "verifyConditionsCmd": "src\\.build\\semantic-release-verify.cmd",
+      "prepareCmd": "src\\.build\\semantic-release-prepare.cmd ${nextRelease.version}",
+      "publishCmd": "src\\.build\\semantic-release-publish.cmd ${nextRelease.version}",
+    }],
+    // github config docs: https://github.com/semantic-release/github
+    ["@semantic-release/github", {
+      "assets": [
+        {"path": "src\\.deploy\\chocolateypackage\\*.nupkg", "label": "NuGet distribution"},
+        {"path": "src\\.deploy\\*.zip", "label": "ZIP distribution"}
+      ]
+    }],
+  ]
+};
---
+++ src/.build/semantic-release-publish.cmd
@@ -0,0 +1,21 @@
+@echo off
+
+REM note: Nodejs configured and semantic-release insalled in appveyor.yml
+REM       This script is called /by/ semantic-release to publish to chocolatey
+
+set THIS_DIR=%~dp0
+
+IF [%1]==[] (
+    ECHO ERROR: Must supply build version as first parameter to this bat file! 1>&2
+    EXIT 1
+)
+set _BUILD_VERSION=%1
+
+ECHO Running choco push...
+
+choco push "%THIS_DIR%..\.deploy\chocolateypackage\lessmsi.%_BUILD_VERSION%.nupkg" --api-key=%CHOCO_KEY%
+
+REM NOTE: ECHO does not clear/set errorlevel https://ss64.com/nt/errorlevel.htmls
+ECHO Running choco push complete. Errorlevel was %ERRORLEVEL%
+
+EXIT %ERRORLEVEL%
