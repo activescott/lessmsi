be4b9f1541d0ddcd4289737aa688720144d3204f
Scott Willeke <scott@willeke.com>
2020-10-10 22:37:39 -0700

    fix(build): more testing with cmd files and semantic-release

--- src/.build/semantic-release-publish.cmd
+++ src/.build/semantic-release-publish.cmd
@@ -3,6 +3,8 @@
 REM note: Nodejs configured and semantic-release insalled in appveyor.yml
 REM       This script is called /by/ semantic-release to publish to chocolatey
 
+set THIS_DIR=%~dp0
+
 IF [%1]==[] (
     ECHO ERROR: Must supply build version as first parameter to this bat file! 1>&2
     EXIT 1
@@ -11,6 +13,9 @@ set _BUILD_VERSION=%1
 
 ECHO Running choco push...
 
-choco push ..\lessmsi.%_BUILD_VERSION%.nupkg --api-key=%CHOCO_KEY%
+choco push "%THIS_DIR%..\.deploy\chocolateypackage\lessmsi.%_BUILD_VERSION%.nupkg" --api-key=%CHOCO_KEY%
 
-ECHO Running choco push complete.
+REM NOTE: ECHO does not clear/set errorlevel https://ss64.com/nt/errorlevel.htmls
+ECHO Running choco push complete. Errorlevel was %ERRORLEVEL%
+
+EXIT %ERRORLEVEL%
