57b364b078f53dca34e22e0501c22eb5505f1f63
scott willeke <scott@willeke.com>
2020-10-12 23:06:24 -0700

    feat: improved error message dialog for all errors; updated message for missing external cabs
    * Improved error message for the specific case of missing external cab file
    * Better UI for users to read error messages/information
    * Users can copy/paste error details
    closes #148

---
+++ src/LessMsi.Gui/Windows.Forms/ErrorDialog.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Forms;
+
+namespace LessMsi.Gui.Windows.Forms
+{
+    public partial class ErrorDialog : Form
+    {
+        public ErrorDialog()
+        {
+            InitializeComponent();
+        }
+
+        public static void ShowError(IWin32Window owner, string message, string technicalDetails)
+        {
+            var dlg = new ErrorDialog();
+            dlg.txtErrorDetail.Text = string.Format("{0}\r\n\r\nTechnical Detail:\r\n\r\n{1}", message, technicalDetails);
+            dlg.ShowDialog(owner);
+        }
+
+        private void lblPleaseReportLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            var target = "https://github.com/activescott/lessmsi/issues/";
+            System.Diagnostics.Process.Start(target);
+        }
+    }
+}
