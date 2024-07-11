a6dbccf879d5f901d2419d7293562c3e3af45e3e
mega5800 <mega5800@gmail.com>
2024-07-11 08:48:15 +0100

    Updataed help text with new flat extraction commands info

--- src/LessMsi.Cli/ShowHelpCommand.cs
+++ src/LessMsi.Cli/ShowHelpCommand.cs
@@ -13,17 +13,19 @@ namespace LessMsi.Cli
 		public static void ShowHelp(string errorMessage)
 		{
 			string helpString =
-				@"Usage:
+                @"Usage:
 lessmsi <command> [options] <msi_name> [<path_to_extract\>] [file_names]
 
 Commands:
-  x  Extracts all or specified files from the specified msi_name.	
-  l  Lists the contents of the specified msi table as CSV to stdout. Table is
-     specified with -t switch. Example: lessmsi l -t Component c:\foo.msi
-  v  Lists the value of the ProductVersion Property in the msi 
-     (typically this is the version of the MSI).
-  o  Opens the specified msi_name in the GUI.
-  h  Shows this help page.
+  x    Extracts all or specified files from the specified msi_name.
+  xfo  Extracts all or specified files from the specified msi_name to the same folder while overwriting files with the same name.
+  xfr  Extracts all or specified files from the specified msi_name to the same folder while renaming files with the same name with a count suffix.
+  l    Lists the contents of the specified msi table as CSV to stdout. Table is
+       specified with -t switch. Example: lessmsi l -t Component c:\foo.msi
+  v    Lists the value of the ProductVersion Property in the msi 
+       (typically this is the version of the MSI).
+  o    Opens the specified msi_name in the GUI.
+  h    Shows this help page.
 
 For more information see http://lessmsi.activescott.com
 ";
