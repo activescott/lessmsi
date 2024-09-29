2518bf69c18e89fba90416b775fb35d34adfd1de
Scott Willeke <scott@willeke.com>
2024-09-29 14:09:23 -0700

    fix: build the gui resources for it lang in msbuild

--- src/clean.bat
+++ src/clean.bat
@@ -2,7 +2,11 @@ rd /S /Q ..\bin
 rd /S /Q .\LessMsi.Core\obj
 rd /S /Q .\LessMsi.Cli\obj
 rd /S /Q .\LessMsi.Gui\obj
+rd /S /Q .\LessMsi.Gui\bin
 rd /S /Q .\ExplorerShortcutHelper\obj
 
 rd /S /Q .\Lessmsi.Tests\bin
 rd /S /Q .\Lessmsi.Tests\obj
+
+
+rd /S /Q .\.deploy
