bae1b8202200dd6b1d75e659e85605b38e8c3f1b
mega5800 <43865655+mega5800@users.noreply.github.com>
2025-01-21 22:13:50 +0000

    feat: Added the ability to change languages in the GUI app (#215)
    * Added the ability to change languages in the GUI app
    
    * Updated the UI logic for better support of dynamic UI updates
    
    * Solved a bug where closing the lang change form would invoke language change
    
    * Amended the code according to Codecy comments
    
    * Amended the code according to Codecy comments
    
    * Amended the code according to Codecy comments 2

---
+++ src/LessMsi.Gui/ChangeLanguageForm.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace LessMsi.Gui
+{
+    partial class ChangeLanguageForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.checkBoxesPanel = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.saveBtn);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 297);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(281, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Location = new System.Drawing.Point(12, 53);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(257, 39);
+            this.saveBtn.TabIndex = 0;
+            this.saveBtn.Text = "Save";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
+            // 
+            // checkBoxesPanel
+            // 
+            this.checkBoxesPanel.Location = new System.Drawing.Point(0, 1);
+            this.checkBoxesPanel.Name = "checkBoxesPanel";
+            this.checkBoxesPanel.Size = new System.Drawing.Size(281, 343);
+            this.checkBoxesPanel.TabIndex = 1;
+            // 
+            // ChangeLanguageForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(281, 397);
+            this.Controls.Add(this.checkBoxesPanel);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ChangeLanguageForm";
+            this.Text = "ChangeLanguageForm";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ChangeLanguageForm_FormClosing);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel checkBoxesPanel;
+        private System.Windows.Forms.Button saveBtn;
+    }
+}
---
+++ src/LessMsi.Gui/ChangeLanguageForm.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Reflection;
+using System.Globalization;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace LessMsi.Gui
+{
+    public partial class ChangeLanguageForm : Form
+    {
+        private bool m_SaveBtnUsed;
+
+        private string m_PreviousCheckedLang;
+        private string m_CurrentCheckedLang;
+
+        private Dictionary<string, CheckBox> m_CheckBoxDict;
+        private Dictionary<string, CultureInfo> m_CultureInfoDict;
+
+        public ChangeLanguageForm()
+        {
+            InitializeComponent();
+
+            m_PreviousCheckedLang = string.Empty;
+            m_CurrentCheckedLang = Thread.CurrentThread.CurrentUICulture.TwoLetterISOLanguageName;
+
+            setGUIData();
+
+            fillCultureInfoDict();
+
+            generateCheckboxes();
+        }
+
+        public string NewSelectedLanguage => m_CurrentCheckedLang;
+
+        private void setGUIData()
+        {
+            Icon = Properties.Resources.LessmsiIcon;
+            Text = Resources.Languages.Strings.ChangeLang;
+        }
+
+        private void fillCultureInfoDict()
+        {
+            m_CultureInfoDict = new Dictionary<string, CultureInfo>();
+
+            string executingAssemblyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var cultureDirectories = Directory.GetDirectories(executingAssemblyPath);
+
+            var cultures = cultureDirectories
+                .Select(Path.GetFileName)
+                .Where(dir => !string.IsNullOrEmpty(dir))
+                .OrderBy(c => c)
+                .ToList();
+
+            cultures.Add("en");
+
+            if (cultures.Any())
+            {
+                foreach (var culture in cultures)
+                {
+                    var cultureInfo = new CultureInfo(culture);
+                    m_CultureInfoDict.Add(culture, cultureInfo);
+                }
+            }
+        }
+
+        private void generateCheckboxes()
+        {
+            checkBoxesPanel.Controls.Clear();
+            m_CheckBoxDict = new Dictionary<string, CheckBox>();
+
+            for (int i = 0; i < m_CultureInfoDict.Count; i++)
+            {
+                var currentCultureKey = m_CultureInfoDict.ElementAt(i).Key;
+                var currentCultureInfo = m_CultureInfoDict.ElementAt(i).Value;
+
+                var checkBox = new CheckBox
+                {
+                    Name = currentCultureKey,
+                    Text = currentCultureInfo.DisplayName,
+                    AutoSize = true,
+                    Margin = new Padding(5),
+                    Location = new System.Drawing.Point(10, 25 * i),
+                    Checked = Thread.CurrentThread.CurrentUICulture.TwoLetterISOLanguageName == currentCultureKey
+                };
+
+                checkBox.Click += (_, e) => OnCheckboxClick(checkBox, e);
+
+                m_CheckBoxDict.Add(currentCultureKey, checkBox);
+
+                checkBoxesPanel.Controls.Add(checkBox);
+            }
+        }
+
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+            m_SaveBtnUsed = true;
+            Close();
+        }
+
+        private void OnCheckboxClick(object sender, EventArgs e)
+        {
+            CheckBox checkBox = sender as CheckBox;
+            if (checkBox != null)
+            {
+                m_PreviousCheckedLang = m_CurrentCheckedLang;
+                m_CurrentCheckedLang = checkBox.Name;
+
+                if (m_CurrentCheckedLang == m_PreviousCheckedLang)
+                {
+                    checkBox.Checked = true;
+                    return;
+                }
+
+                m_CheckBoxDict[m_PreviousCheckedLang].Checked = false;
+            }
+        }
+
+        private void ChangeLanguageForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!m_SaveBtnUsed)
+            {
+                m_CurrentCheckedLang = string.Empty;
+            }
+        }
+    }
+}
