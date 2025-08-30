1f7031ccee5cb5337c2461f969030652f34531e6
mega5800 <mega5800@gmail.com>
2025-08-30 14:37:11 +0300

    amended the code according to Codacy's comments 1

--- src/ExplorerShortcutHelper/ExplorerShortcutHelper.cs
+++ src/ExplorerShortcutHelper/ExplorerShortcutHelper.cs
@@ -55,17 +55,18 @@ namespace Willeke.Scott.ExplorerShortcutHelper
                 return CommandLineError("Invalid argument. Expected a file class.");
             }
 
-            //e.g. "Msi.Package";
-            var fileClass = args[2];
+            var fileClass = args[2]; //e.g. "Msi.Package"
 
             if (isAddingKey.Value)
-            {   
+            {
                 // if we're adding a key we're expecting more arguments:
                 if (args.Length < 3)
+                {
                     return CommandLineError("Invalid argument. Expected caption.");
+                }
 
-                var contextMenuCaption = args[3]; // e.g. "&Extract Files";
-                var shellCommandToExecute = args[4]; //e.g. '\"' + GetExePath() + "\" /x \"%1\" \"%1_extracted\"";
+                var contextMenuCaption = args[3]; // e.g. "&Extract Files"
+                var shellCommandToExecute = args[4]; //e.g. '\"' + GetExePath() + "\" /x \"%1\" \"%1_extracted\""
 
                 var message = string.Format(CultureInfo.InvariantCulture,
                                         "Adding a shortcut for '{0}' files that will execute the following command: '{1}'...",
