54a142ac5dd9e0f92f21151e49588244b7e334ac
mega5800 <mega5800@gmail.com>
2025-10-07 17:25:54 +0300

    updated the help menu

--- src/LessMsi.Cli/ShowHelpCommand.cs
+++ src/LessMsi.Cli/ShowHelpCommand.cs
@@ -18,12 +18,13 @@ lessmsi <command> [options] <msi_name> [<path_to_extract\>] [file_names]
 
 Commands:
   x    Extracts all or specified files from the specified msi_name.
+  xo   Extracts all or specified files from the specified msi_name while overwriting files with the same name.
   xfo  Extracts all or specified files from the specified msi_name to the same folder while overwriting files with the same name.
   xfr  Extracts all or specified files from the specified msi_name to the same folder while renaming files with the same name with a count suffix.
-  l    Lists the contents of the specified msi table as CSV to stdout. Table is
-       specified with -t switch. Example: lessmsi l -t Component c:\foo.msi
-  v    Lists the value of the ProductVersion Property in the msi 
-       (typically this is the version of the MSI).
+  l    Lists the contents of the specified msi table as CSV to stdout. 
+       Table is specified with -t switch. 
+       Example: lessmsi l -t Component c:\foo.msi
+  v    Lists the value of the ProductVersion Property in the msi (typically this is the version of the MSI).
   o    Opens the specified msi_name in the GUI.
   h    Shows this help page.
 
