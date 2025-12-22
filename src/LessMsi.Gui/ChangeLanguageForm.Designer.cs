0a551bedb80c94ecb853d1615c4fb3c2db897692
JFX <1024tools@gmail.com>
2025-12-22 12:33:24 +0800

    Replace checkBoxesPanel with radioButtonsPanel and reset the change language form to its original default size
    Replace checkBoxesPanel with radioButtonsPanel and reset the change language form to its original default size

--- src/LessMsi.Gui/ChangeLanguageForm.Designer.cs
+++ src/LessMsi.Gui/ChangeLanguageForm.Designer.cs
@@ -30,7 +30,7 @@ namespace LessMsi.Gui
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.saveBtn = new System.Windows.Forms.Button();
-            this.checkBoxesPanel = new System.Windows.Forms.Panel();
+            this.radioButtonsPanel = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             
@@ -38,18 +38,16 @@ namespace LessMsi.Gui
             // panel1 (Bottom button area)
             // 
             this.panel1.Controls.Add(this.saveBtn);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom; // Dock to bottom
-            this.panel1.Location = new System.Drawing.Point(0, 397);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 297);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(400, 80); // Reduced height slightly for a more compact look
+            this.panel1.Size = new System.Drawing.Size(281, 100);
             this.panel1.TabIndex = 0;
             
             // 
             // saveBtn
             // 
-            // Center the button
-            this.saveBtn.Anchor = System.Windows.Forms.AnchorStyles.None; 
-            this.saveBtn.Location = new System.Drawing.Point(72, 20); 
+            this.saveBtn.Location = new System.Drawing.Point(12, 53);
             this.saveBtn.Name = "saveBtn";
             this.saveBtn.Size = new System.Drawing.Size(257, 39);
             this.saveBtn.TabIndex = 0;
@@ -58,28 +56,27 @@ namespace LessMsi.Gui
             this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
             
             // 
-            // checkBoxesPanel (Language list area)
+            // radioButtonsPanel (Language list area)
             // 
-            this.checkBoxesPanel.Dock = System.Windows.Forms.DockStyle.Fill; // Fill remaining space
-            this.checkBoxesPanel.AutoScroll = true; // [Key Change] Enable auto-scrolling
-            this.checkBoxesPanel.Location = new System.Drawing.Point(0, 0);
-            this.checkBoxesPanel.Name = "checkBoxesPanel";
-            this.checkBoxesPanel.Size = new System.Drawing.Size(400, 397);
-            this.checkBoxesPanel.TabIndex = 1;
+            this.radioButtonsPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.radioButtonsPanel.AutoScroll = true;
+            this.radioButtonsPanel.Location = new System.Drawing.Point(0, 0);
+            this.radioButtonsPanel.Name = "radioButtonsPanel";
+            this.radioButtonsPanel.Size = new System.Drawing.Size(281, 343);
+            this.radioButtonsPanel.TabIndex = 1;
             
             // 
             // ChangeLanguageForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            // [Key Change] Increase width to 400 (was 291) to ensure content isn't cut off
-            this.ClientSize = new System.Drawing.Size(400, 480); 
-            this.Controls.Add(this.checkBoxesPanel);
+            this.ClientSize = new System.Drawing.Size(281, 397);
+            this.Controls.Add(this.radioButtonsPanel);
             this.Controls.Add(this.panel1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog; // Changed to FixedDialog to look more like a dialog
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent; // Suggestion: Center on parent
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Name = "ChangeLanguageForm";
             this.Text = "Change Language";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ChangeLanguageForm_FormClosing);
@@ -91,7 +88,7 @@ namespace LessMsi.Gui
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel checkBoxesPanel;
+        private System.Windows.Forms.Panel radioButtonsPanel;
         private System.Windows.Forms.Button saveBtn;
     }
 }
